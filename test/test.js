3a830794cfcd00c8b31c9e3b0bc6a666a49174ed
Sitnitsa Pavel <vip.sitnitsa@mail.ru>
2021-03-28 20:01:27 +0300

    commit-1

---
+++ test/test.js
@@ -0,0 +1,26 @@
+const chai = require('chai'); 
+function pow(x, n) {
+  let result = 1;
+
+  for (let i = 0; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+describe("pow", function () {
+  function makeTest(x) {
+    let expected = x * x * x;
+    console.log('expected :>> ', expected);
+    it(`${x} в степени 3 будет ${expected}`, function() {
+      chai.expect(pow(x, 3)).equal(expected);
+    });
+  }
+
+  for (let x = 1; x <= 5; x++) {
+    makeTest(x);
+  }
+});
+
+// я тут был
+//и я тут был
