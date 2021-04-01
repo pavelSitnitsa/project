9de4971b90a312e6d660087b19a5905ce87ea96d
Sitnitsa Pavel <vip.sitnitsa@mail.ru>
2021-04-01 10:32:50 +0300

    Test-2

--- test/test.js
+++ test/test.js
@@ -1,24 +1,27 @@
 const chai = require("chai");
 const axios = require("axios");
 describe("testing HTTP request", function () {
+  it("post resting", function () {
+    axios
+      .post("https://jsonplaceholder.typicode.com/posts")
+      .then(function (response) {
+        
+        chai.expect(101).equals(response.data.id);
+        chai.expect(201).equals(response.status)
+      });
+  });
+
   it(`get testing`, function () {
     // Make a request for a user with a given ID
     axios
       .get("https://reqres.in/api/users/2")
 
       .then(function (response) {
-        chai
-        .expect(200)
-        .equals(response.status)
+        chai.expect(200).equals(response.status);
         let firstName = response.data.data.first_name;
-        chai
-        .expect("Janet")
-        .equals(firstName)
+        chai.expect("Janet").equals(firstName);
         let lastName = response.data.data.last_name;
-        chai
-        .expect("Weaver")
-        .equals(lastName)
-
+        chai.expect("Weaver").equals(lastName);
       })
       .catch(function (error) {
         // handle error
